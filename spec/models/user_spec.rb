79d510a1cf7f938b394fa23c9f53a5294ec0e4d0
gianniresende <gianniresende@gmail.com.br>
2025-06-28 09:20:12 -0300

    feat: add tests to uuid and enums

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,6 +23,25 @@ RSpec.describe User, type: :model do
     end
   end
 
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).scoped_to(:provider).case_insensitive }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+  end
+
+  describe 'UUID as primary key' do
+    it 'has a valid UUID as id' do
+      user = create(:user)
+      expect(user.id).to be_present
+      expect(user.id).to match(/\A[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/i)
+    end
+  end
+
+  describe 'enums' do
+    it { is_expected.to define_enum_for(:role).with_values(%i[user admin]) }
+    it { is_expected.to define_enum_for(:status).with_values(%i[active inactive]) }
+  end
+
   describe '#admin?' do
     context 'when user is admin' do
       it 'return true' do
