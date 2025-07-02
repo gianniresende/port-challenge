67adca823a57caf9ab42fedcef12cca4a99b1812
gianniresende <gianniresende@gmail.com.br>
2025-07-02 10:04:34 -0300

    feat: backend add rspec to api_token

--- backend/app/models/api_token.rb
+++ backend/app/models/api_token.rb
@@ -9,7 +9,7 @@ class ApiToken < ApplicationRecord
   private
 
   def generate_secure_token
-    self.token = SecureRandom.hex(32)
+    self.token ||= SecureRandom.hex(32)
   end
 
   def set_last_used_at
